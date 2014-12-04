f384b05f5da742061aec7b49326ffe6fc9071098
Hector Rios <labeledloser@gmail.com>
2014-12-04 17:22:55 -0600

    Reduced the swipe threshold.

--- js/animations.js
+++ js/animations.js
@@ -5,12 +5,27 @@ hotOrNot.animations = (function() {
   var stack,
     cards;
 
+  /**
+   * This configures Swing.
+   * when throwOutConfidence === Math.abs(1), it moves the card.
+   * This allows us to reduce the accuracy.
+   */
+  var getConfig = function() {
+    var config = {
+      throwOutConfidence: function(offset, element) {
+        return Math.min(Math.abs(offset * 3) / element.offsetWidth, 1);
+      }
+    };
+
+    return config;
+  };
+
   var init = function () {
     // Prepare the cards
     cards = [].slice.call(document.querySelectorAll('ul li'));
 
     // An instance of the Stack is used to attach event listeners
-    stack = gajus.Swing.Stack();
+    stack = gajus.Swing.Stack(getConfig());
 
     cards.forEach(function (targetElement) {
       stack.createCard(targetElement);
@@ -18,15 +33,14 @@ hotOrNot.animations = (function() {
 
     // Add an event listener for when a card is moved from the stack
     stack.on('throwout', function(e) {
-      console.log('Throwout event!');
       if (e.throwDirection == gajus.Swing.Card.DIRECTION_RIGHT) {
         $(document).trigger({ type: 'LISTING_LIKED', listing: e.target});
       }
     });
 
-    stack.on('thrown', function(e) {
-      console.log('Card has snapped back to the stack.');
-    });
+    //stack.on('thrown', function(e) {
+      //console.log('Card has snapped back to the stack.');
+    //});
   };
 
   return {
